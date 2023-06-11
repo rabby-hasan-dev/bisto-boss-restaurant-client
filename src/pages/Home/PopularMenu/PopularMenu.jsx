2143b4ed9a37b3f0977668e25ca26825d0d17187
rabby6530 <you@example.com>
2023-06-11 16:50:30 +0600

    use custom hook

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,29 +1,25 @@
 import { useEffect, useState } from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import MenuItem from "../../Shared/MenuItem/MenuItem";
+import useMenu from "../../../hooks/useMenu";
 
 const PopularMenu = () => {
-    const [menu,setMenu]=useState([])
-    
-    useEffect(()=>{
-        fetch('menu.json')
-        .then(res=>res.json())
-        .then(data=>{
-            const popularItems=data.filter(item=>item.category==="popular");
-            setMenu(popularItems);
-        })
-    },[])
+    const [menu] = useMenu();
+    const popular = menu.filter(item => item.category === "popular");
+
     return (
         <section className="mb-12">
             <SectionTitle
-            subHeading={"Check it out"}
-            heading={"FROM OUR MENU"}
+                subHeading={"Check it out"}
+                heading={"FROM OUR MENU"}
             ></SectionTitle>
             <div className="grid md:grid-cols-2 gap-10">
-                {menu.map(item=> <MenuItem
-                key={item._id}
-                item={item}
-                ></MenuItem>)}
+                {
+                    popular.map(item => <MenuItem
+                        key={item._id}
+                        item={item}
+                    ></MenuItem>)
+                }
             </div>
             <div className="md:w-3/12 mx-auto text-center mt-16">
                 <button className="btn btn-outline border-0 border-b-4">View full menu</button>
--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,7 +1,7 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../../Shared/Cover/Cover";
 import menuImg from '../../../assets/menu/menu-bg.jpg'
-import PopularMenu from "../../Home/PopularMenu/PopularMenu";
+
 
 
 const Menu = () => {
@@ -11,11 +11,7 @@ const Menu = () => {
                 <title>Bistro Boss| Menu</title>
             </Helmet>
             <Cover img={menuImg} title={"our menu"} ></Cover>
-            <PopularMenu></PopularMenu>
-            <Cover img={menuImg} title={"our menu"} ></Cover>
-            <PopularMenu></PopularMenu>
-            <Cover img={menuImg} title={"our menu"} ></Cover>
-            <PopularMenu></PopularMenu>
+           
         </div>
     );
 };
